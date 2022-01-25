a279a1bcce6d3c17dca50d9b01a28690b15d47b2
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-25 10:34:19 -0800

    region_spec.rb Add :region helper function for Region instantiation

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+    let(:region) {Region.new}
+    
     it "exists" do
       Region.new
     end
 
     describe "attributes" do
       it "has a name" do
-        expect(Region.new).to respond_to(:name)
+        expect(region).to respond_to(:name)
       end
     end
 
