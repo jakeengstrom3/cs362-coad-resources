e89dc2a9f186ccbf45b5360460f3905678919fe7
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-25 10:30:33 -0800

    region_spec.rb Add Has a Name attribute test

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Region, type: :model do
       Region.new
     end
 
+    describe "attributes" do
+      it "has a name" do
+        expect(Region.new).to respond_to(:name)
+      end
+    end
+
 end
