acebfbe204aa4fd5ac46586c0bf979fcc5e95f0d
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-25 11:26:45 -0800

    region_spec.rb Add validates name length test. Refactored has a name test to validates presence of name using shoulda matcher

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-    let(:region) {Region.new}
-    
-    it "exists" do
-      Region.new
+  let(:region) {Region.new}
+
+  it "exists" do
+    region
+  end
+
+  describe "attributes" do
+    it "validates presence of name" do
+      expect(region).to validate_presence_of(:name)
     end
 
-    describe "attributes" do
-      it "has a name" do
-        expect(region).to respond_to(:name)
-      end
+    it "validates name length" do
+      expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255)
     end
+    
+   
+  end
 
 end
