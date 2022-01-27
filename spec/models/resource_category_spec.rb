ce241d06b40c6081610ab230ba3dd332659d6067
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-27 11:39:57 -0800

    Add validation tests for the following: presence of name name length name uniqueness has many tickets

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe ResourceCategory, type: :model do
     ResourceCategory.new
   end
 
+  it "validates presence of name" do
+    expect(region).to validate_presence_of(:name)
+  end
+
+  it "validates name length" do
+    expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255)
+  end
+  
+  it "validates name uniqueness" do
+    expect(region).to validate_uniqueness_of(:name).case_insensitive
+  end
+
+  it "checks if it has many tickets" do
+    expect(region).to have_many(:tickets)
+  end
+  
 end
