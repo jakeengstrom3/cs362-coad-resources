6513f600599f80bf241383295af807eddc96af4e
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-01-31 10:57:58 -0800

    user_spec.rb Add instantiaion and validates presence of email tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
+  let (:user) {User.new(email: "FAKE_EMAIL@FAKE.FAKE",
+                        password: "FAKE_PASSWORD")}
+
+  it "exits" do
+    user
+  end
+
+  describe "attributes" do
+    it "validates presence of email" do
+      expect(user).to validate_presence_of(:email)
+    end
+  end
+
 
 end
