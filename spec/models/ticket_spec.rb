7fa1fdcfaa153203dab33d2b674f7742ab135f42
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-02-01 08:38:38 -0800

    ticket_spec Add most attribute tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,38 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-end
+  let(:ticket) {Ticket.new(name: "FAKE NAME",
+  phone: "FAKE PHONE",
+  region_id: "FAKE REGION ID",
+  resource_category_id: "FAKE RESOURCE CATEGORY ID" )} 
+
+  describe "attributes" do
+    it "belongs to a region" do
+      expect(ticket).to belong_to(:region);
+    end
+    
+    it "belongs to resource category" do
+      expect(ticket).to belong_to(:resource_category)
+    end
+    
+    it "belongs to an organization" do
+      expect(ticket).to belong_to(:organization)
+    end
+
+    it "validates presence of name, phone, region id, and resource_category" do
+      expect(ticket).to validate_presence_of(:name)
+      expect(ticket).to validate_presence_of(:phone)
+      expect(ticket).to validate_presence_of(:region_id)
+      expect(ticket).to validate_presence_of(:resource_category_id)
+    end
+
+    it "validates length of name" do
+      expect(ticket).to validate_length_of(:name).is_at_least(1).is_at_most(255)
+    end
+
+    it "validates length of description" do
+      expect(ticket).to validate_length_of(:description).is_at_most(1020)
+    end
+  end
+
+end 
