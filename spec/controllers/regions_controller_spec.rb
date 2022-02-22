8045858a35d146d034bf85f417f46912074afb92
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-02-22 11:51:15 -0800

    got regions controller edit and show under tests

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe RegionsController, type: :controller do
             expect(response).to redirect_to(new_user_session_path)
             get :new
             expect(response).to redirect_to(new_user_session_path)
+            get :edit, params:{id:"FAKE"}
+            expect(response).to redirect_to(new_user_session_path)
+            get :show, params:{id:"FAKE"}
+            expect(response).to redirect_to(new_user_session_path)
         end
     end
 end
