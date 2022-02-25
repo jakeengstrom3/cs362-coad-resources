8ef0c71951199a6786d23103e85a78344edc2d6d
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-02-25 12:43:56 -0800

    got orgs controller function reject under test

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe OrganizationsController, type: :controller do
             expect(response).to redirect_to(new_user_session_path)
             post :approve, params:{id:"FAKE"}
             expect(response).to redirect_to(new_user_session_path)
+            post :reject, params:{id:"FAKE"}
+            expect(response).to redirect_to(new_user_session_path)
         end
     end
 end
