3afd98f581f78fa4fe4602ac5740c1945d148006
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-02-25 12:28:29 -0800

    got organizations index and new under test

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+    describe "A user who is not logged in" do
+        it "Redirects to sign-in screen" do
+            get :index
+            expect(response).to redirect_to(new_user_session_path)
+            get :new
+            expect(response).to redirect_to(new_user_session_path)
+        end
+    end
 end
