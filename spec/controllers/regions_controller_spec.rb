81cad2fc6f1a4b72fc9833ae830653c7c0ba3096
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-02-25 12:04:19 -0800

    Update regions_controller_spec.rb

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -22,4 +22,29 @@ RSpec.describe RegionsController, type: :controller do
             expect(response).to redirect_to(new_user_session_path)
         end
     end
+
+    # describe "A user who is not logged in" do
+    #     it "Redirects to sign-in screen" do
+    #         orginization_user = create(:user)
+    #             orginization_user.confirm
+    #             sign_in(orginization_user)
+
+    #         get :index
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         get :new
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         get :edit, params:{id:"FAKE"}
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         get :show, params:{id:"FAKE"}
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         post :create
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         delete :destroy, params:{id:"FAKE"}
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         patch :update, params:{id:"FAKE"} 
+    #         expect(response).to redirect_to(new_user_session_path)
+    #         put :update, params:{id:"FAKE"}
+    #         expect(response).to redirect_to(new_user_session_path)
+    #     end
+    # end
 end
