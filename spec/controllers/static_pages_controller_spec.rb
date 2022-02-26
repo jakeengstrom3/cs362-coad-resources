485b9c8444816abbcafc09ed16ba33b459346f7a
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-02-25 17:27:25 -0800

    tried tickets and static pages but they possibly need mocks to be done

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+    # describe "A user who is not logged in" do
+    #     it "Redirects to sign-in screen" do
+    #         get :index
+    #         expect(response).to redirect_to(new_user_session_path)
+    #     end
+    # end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-    
+    # describe "A user who is not logged in" do
+    #     it "Redirects to sign-in screen" do
+    #         get :new
+    #         expect(response).to redirect_to (:new_user_session_path)
+    #     end
+    # end
 end
