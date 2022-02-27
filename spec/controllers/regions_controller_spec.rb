3d1526762fe2ed93dcd1cd7bbe99bccd3ac9935a
Peter Storum <p-storum@hotmail.com>
2022-02-27 15:22:31 -0800

    fixed an issue with regions controller where it was creating before a proper factory was in place

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -23,28 +23,28 @@ RSpec.describe RegionsController, type: :controller do
         end
     end
 
-    # describe "A user who is not logged in" do
-    #     it "Redirects to sign-in screen" do
-    #         orginization_user = create(:user)
-    #             orginization_user.confirm
-    #             sign_in(orginization_user)
+     describe "A user who is logged in" do
+         it "Redirects to proper page" do
+            orginization_user = create(:user, :make_org)
+            orginization_user.confirm
+            sign_in(orginization_user)
 
-    #         get :index
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         get :new
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         get :edit, params:{id:"FAKE"}
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         get :show, params:{id:"FAKE"}
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         post :create
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         delete :destroy, params:{id:"FAKE"}
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         patch :update, params:{id:"FAKE"} 
-    #         expect(response).to redirect_to(new_user_session_path)
-    #         put :update, params:{id:"FAKE"}
-    #         expect(response).to redirect_to(new_user_session_path)
-    #     end
-    # end
+             get :index
+             expect(response).to redirect_to(dashboard_path)
+             get :new
+             expect(response).to redirect_to(dashboard_path)
+             get :edit, params:{id:"FAKE"}
+             expect(response).to redirect_to(dashboard_path)
+             get :show, params:{id:"FAKE"}
+             expect(response).to redirect_to(dashboard_path)
+             post :create
+             expect(response).to redirect_to(dashboard_path)
+             delete :destroy, params:{id:"FAKE"}
+             expect(response).to redirect_to(dashboard_path)
+             patch :update, params:{id:"FAKE"} 
+             expect(response).to redirect_to(dashboard_path)
+             put :update, params:{id:"FAKE"}
+             expect(response).to redirect_to(dashboard_path)
+         end
+     end
 end
