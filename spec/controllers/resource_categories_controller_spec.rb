18e86d3f780842669778affb070c11e92854df43
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-02-26 16:26:40 -0800

    resource_categories_controller finished non-logged in user redirect tests

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -10,7 +10,20 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       get :edit, params:{id: "FAKE ID"}
       expect(response).to redirect_to(new_user_session_path)
-      
+      get :show, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :activate, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path )
+      patch :deactivate, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
+      post :create, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
+      put :update, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
+      delete :destroy, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
