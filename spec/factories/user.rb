a6aabc281ba8ce1a8577424bc9952266d79b314d
Peter Storum <p-storum@hotmail.com>
2022-02-27 15:14:58 -0800

    updated the user factory to attach an organization when a user is made for an organization

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,14 +1,16 @@
 FactoryBot.define do
   factory :user do
-    email {"FakeEmail"}
-    password {"FakePW"}
+    email {'FakeEmail@fake.com'}
+    password {'FakePW123'}
+    password_confirmation {'FakePW123'}
 
     trait :make_admin do
       role {:admin}
     end
 
     trait :make_org do
-      role {:organization}
+      role {'organization'}
+      organization
     end
   end
 end
