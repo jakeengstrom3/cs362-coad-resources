49f1f7840ee86690ce2bbc0a9acaf075962d5e86
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-02-26 16:21:03 -0800

    controller_spec started testing non signed in user redirects

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+  describe "A user who is not signed in as Admin" do
+    it "redirects to sign in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params:{id: "FAKE ID"}
+      expect(response).to redirect_to(new_user_session_path)
+      
+    end
+  end
+
 end
