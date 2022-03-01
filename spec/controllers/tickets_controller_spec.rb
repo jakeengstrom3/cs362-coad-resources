ce2645a9cf5b5fb7c781949d50156f92ebff5f8f
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-03-01 11:53:07 -0800

    added unapporved organization test cases

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -6,16 +6,45 @@ RSpec.describe TicketsController, type: :controller do
         it "Redirects to dashboard" do
             get :show, params: {id: 'FAKE'}
             expect(response).to redirect_to(dashboard_path)
+            post :capture, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
+            post :release, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
+            patch :close, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
             delete :destroy, params:{id:"FAKE"}
             expect(response).to redirect_to(dashboard_path)
         end
     end
 
-    describe "An organization user" do
+    describe "An approved organization user" do
         it "Redirects to dashboard" do
             orginization_user = create(:user, :make_org)
             orginization_user.confirm
+            orginization_user.organization.approve
             sign_in(orginization_user)
+
+            delete :destroy, params:{id:"FAKE"}
+            expect(response).to redirect_to(dashboard_path)
+        end
+    end
+
+    describe "An un-approved organization user" do
+        it "rediects to dashboard" do
+            orginization_user = create(:user, :make_org)
+            orginization_user.confirm
+            orginization_user.organization.reject
+            orginization_user.organization.save!
+            sign_in(orginization_user)
+            
+            get :show, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
+            post :capture, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
+            post :release, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
+            patch :close, params: {id: 'FAKE'}
+            expect(response).to redirect_to(dashboard_path)
             delete :destroy, params:{id:"FAKE"}
             expect(response).to redirect_to(dashboard_path)
         end
