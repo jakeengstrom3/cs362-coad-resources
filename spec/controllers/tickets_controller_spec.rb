a08511d002c0018a7a1d47a9ca8df3351665ec7c
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-03-01 11:31:32 -0800

    I wrote down what we need to start testing tickets controller

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-    # describe "A user who is not logged in" do
-    #     it "Redirects to sign-in screen" do
-    #         get :new
-    #         expect(response).to redirect_to (:new_user_session_path)
-    #     end
-    # end
+    describe "A user who is not logged in" do
+        it "Redirects to sign-in screen" do
+            expect { get:show, params,: {id:"FAKE"}}.to raise_error
+            get :new
+            expect(response).to redirect_to (dashboard_path)
+        end
+    end
 end
