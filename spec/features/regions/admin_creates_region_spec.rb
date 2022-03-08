4ebd0b3cd75e1357b3492eb8f3a9b085256d381e
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-03-08 10:19:55 -0800

    Update admin_creates_region_spec.rb
    got it fixed and running jake helped me

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -5,23 +5,23 @@ RSpec.describe 'Creating a Region', type: :feature do
     scenario "with valid atributes succeeds" do
         user = create(:user, :admin)
         user.confirm
-        logn_in_as(user)
+        log_in_as(user)
         visit new_region_url
         fill_in "Name", with: "FAKE Region"
         click_on "Add Region"
         expect(page).to have_text "Region successfully created"
-        expect(page).to have_text "Fake Region"
+        expect(page).to have_text "FAKE Region"
     end
 
     scenario "with invalid atributes shows error" do
         user = create(:user, :admin)
         user.confirm
-        logn_in_as(user)
+
+        log_in_as(user)
         visit new_region_url
         fill_in "Name", with: ""
         click_on "Add Region"
         expect(page).to have_text "errors prohibited this region"
-        expect(page).to have_text "Fake Region"
     end
 
 
