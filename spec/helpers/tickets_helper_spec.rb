b404b7d41a457c13d370ced02cbb6d3ac36c00fd
piedude1717 <62221878+piedude1717@users.noreply.github.com>
2022-03-08 11:32:52 -0800

    Update tickets_helper_spec.rb
    got ticket helper commented code to work

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the TIcketsHelper. For example:
+# the TicketsHelper. For example:
 #
-# describe TIcketsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
+describe TicketsHelper do
+  describe "string concat" do
+    it "concats two strings with spaces" do
+      combined_string = ("this "+"that")
+      expect(combined_string).to eq("this that")
+    end
+  end
+end
 RSpec.describe TicketsHelper, type: :helper do
   describe "format_phone_number" do
     it "formats phone number" do
