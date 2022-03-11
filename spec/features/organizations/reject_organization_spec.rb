4b8d5537d7f7ef27572a116eaa72ddd689ff6298
Peter Storum <p-storum@hotmail.com>
2022-03-11 11:31:09 -0800

    renamed variables for clairity

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Rejecting an organization', type: :feature do
 
     let(:admin) {create(:user, :make_admin)}
-    let(:organization_rejected) {create(:organization, :set_submitted)}
+    let(:organization_submitted) {create(:organization, :set_submitted)}
 
-    scenario "admin rejects an organization" do
+    scenario "admin rejects a submitted organization" do
         admin.confirm
         log_in_as(admin)
-        visit organization_path(organization_rejected)
+        visit organization_path(organization_submitted)
         click_on "Reject"
         expect(page).to have_text('has been rejected')
     end
