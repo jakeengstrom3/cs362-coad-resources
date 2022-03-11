d6509f3e59bc19f7e2a7383e610b67830b9c6f30
Peter Storum <p-storum@hotmail.com>
2022-03-11 11:23:04 -0800

    create admin that rejects an organization

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+    let(:admin) {create(:user, :make_admin)}
+    let(:organization_rejected) {create(:organization, :set_submitted)}
+
+    scenario "admin rejects an organization" do
+        admin.confirm
+        log_in_as(admin)
+        visit organization_path(organization_rejected)
+        click_on "Reject"
+        expect(page).to have_text('has been rejected')
+    end
 end
