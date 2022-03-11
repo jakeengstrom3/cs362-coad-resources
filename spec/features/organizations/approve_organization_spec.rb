21350e158533e59380d4f754dc2f46931dfe8ee1
Peter Storum <p-storum@hotmail.com>
2022-03-11 11:18:56 -0800

    creates an admin that approves an organization

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+    let(:admin) {create(:user, :make_admin)}
+    let(:organization_submitted) {create(:organization, :set_submitted)}
+
+    scenario "admin approves an organization" do
+        admin.confirm
+        log_in_as(admin)
+        visit organization_path(organization_submitted)
+        click_on "Approve"
+        expect(page).to have_text('has been approved')
+    end
 end
