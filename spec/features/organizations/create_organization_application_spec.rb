647e5bd6bfcf88ee14d79062381e6676c11645bf
Peter Storum <p-storum@hotmail.com>
2022-03-12 13:27:29 -0800

    system test for an application that was not filled out correctly

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -29,7 +29,33 @@ RSpec.describe 'Creating an Organization Application', type: :feature do
         fill_in 'organization_description', with: 'fake description'
         choose 'organization_transportation_yes'
         click_on "Apply"
-        expect(page).to have_text("Application Submitted") 
-    	
+        expect(page).to have_text("Application Submitted")
+    end
+
+    scenario "new org Application that forgot to fill in email requirement" do
+        user.confirm
+        log_in_as(user)
+        create(:user, :make_admin)
+        visit new_organization_application_path
+        choose "organization_liability_insurance_true"
+        choose "organization_agreement_one_true"
+        choose "organization_agreement_two_true"
+        choose "organization_agreement_three_true"
+        choose "organization_agreement_four_true"
+        choose "organization_agreement_five_true"
+        choose "organization_agreement_six_true"
+        choose "organization_agreement_seven_true"
+        choose "organization_agreement_eight_true"
+        fill_in 'organization_primary_name', with: 'fake'
+        fill_in 'organization_name', with: 'fake'
+        fill_in 'organization_title', with: 'fake'
+        fill_in 'organization_phone', with: '0000000000'
+        fill_in 'organization_secondary_name', with: 'fake'
+        fill_in 'organization_secondary_phone', with: '0000000000'
+        fill_in 'organization_primary_name', with: 'fake'
+        fill_in 'organization_description', with: 'fake description'
+        choose 'organization_transportation_yes'
+        click_on "Apply"
+        expect(page).to have_text("Email can't be blank")
     end
 end
